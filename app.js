5a085189f309a4eefe0277468619ba3c2c8abbd3
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-01 16:29:35 +0900

    [fix] app.js 수정

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ app.use("/public", express.static(__dirname + "/public"));
 const server = http.createServer(app);
 const PORT = 3000;
 
-app.get("", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
