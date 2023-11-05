69a0338e4ae181169945b7085f11a1438ba6f96f
Byun Jung Won <labhg64@gmail.com>
2023-11-06 03:04:08 +0900

    크롤링 개선 완료(진)

--- modules/common/mongodb/mongodbInsertData.js
+++ modules/common/mongodb/mongodbInsertData.js
@@ -1,10 +1,31 @@
+const processDocument  = (doc) => ({
+      poster: doc.poster || null,
+      title: doc.title || null,
+      venue: doc.venue || null,
+      location: doc.location || null,
+      tel: doc.tel || null, 
+      explanation: doc.explanation || null,
+      level: doc.level || null,
+      timeLimit: doc.duration || null,
+      price: doc.price || null,
+      minHeadcount: doc.minHeadcount || null,
+      maxHeadcount: doc.maxHeadcount || null,
+      genre: doc.themeGenre || null,
+      activity: doc.activityLevel || null,
+      horror: doc.fearLevel || null,
+      lockRatio: doc.deviceRatio || null,
+      reservationNotice: doc.reservationNotice || null
+  });
+
 const mongodbInsertData = async (bid, data, collection) => {
+  const processedData = data.map(doc => processDocument(doc));
+
   const existingDataCount = await collection.countDocuments({ bid: bid });
 
   if (isDataExist(existingDataCount)) {
-    await collection.updateMany({ bid: bid }, { $set: data[0] });
+    await collection.updateMany({ bid: bid }, { $set: processedData[0] });
   } else {
-    await collection.insertMany(data);
+    await collection.insertMany(processedData);
   }
 };
 
