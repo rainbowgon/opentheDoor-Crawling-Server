6295f433f03f483eca5091513f49ef852bde9aec
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 03:25:35 +0900

    [fix] dotenv path 명시

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "./.env" });
 
 import run from "./modules/informationCrawlers/masterkey/run.js";
 import express from "express";
@@ -11,6 +11,7 @@ import { MONGODB_URL } from "./modules/common/config/env.js";
 const app = express();
 
 console.log(MONGODB_URL);
+console.log(process.env.MONGODB_URL);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use("/public", express.static(__dirname + "/public"));
