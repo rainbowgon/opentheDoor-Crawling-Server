05cc48e2a0165f7fbb50430168bb1353828548ed
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 03:28:35 +0900

    [fix] dotenv 수정

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config({ path: "./.env" });
-
 import run from "./modules/informationCrawlers/masterkey/run.js";
 import express from "express";
 import http from "http";
@@ -11,7 +7,6 @@ import { MONGODB_URL } from "./modules/common/config/env.js";
 const app = express();
 
 console.log(MONGODB_URL);
-console.log(process.env.MONGODB_URL);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use("/public", express.static(__dirname + "/public"));
