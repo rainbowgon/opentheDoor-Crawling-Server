12f7cdaec90d066dbd4d53eb543dd6b400acc69c
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-18 22:18:12 +0900

    [fix] 빈자리 알림

--- src/common/redis/redisTaskCreator.js
+++ src/common/redis/redisTaskCreator.js
@@ -1,18 +1,66 @@
-import flattenTimeLine from "../tools/flattenTimeLine.js";
+import AvailableStatus from "../tools/enum.js";
+import { flattenObjectList, flattenTimeLineObject } from "../tools/flatten.js";
+import { createHash } from "../tools/hashCreator.js";
 
 const createInsertDataTask = (timeLine, redisClient) => {
   return new Promise(async (resolve, reject) => {
     const previousData = await redisClient.hgetall("TimeLine:" + timeLine.timeLineId);
 
-    if (check(previousData, timeLine)) {
+    if (isNotChanged(previousData, timeLine)) {
       resolve();
       return;
     }
 
-    await redisClient.hmset("TimeLine:" + timeLine.timeLineId, ...flattenTimeLine(timeLine));
+    const flattened = flattenTimeLineObject(timeLine);
+    checkEmptyTimeSlot(previousData, flattened);
+
+    await redisClient.hmset("TimeLine:" + timeLine.timeLineId, ...flattenObjectList(flattened));
   });
 };
 
-const check = (previous, current) => Object.keys(previous).length !== 0;
+const isNotChanged = (previous, current) => {
+  createHash(previous) === createHash(current);
+};
+
+const checkEmptyTimeSlot = (previousTimeLine, currentTimeLine) => {
+  const previousStatusKeys = filterStatusKeys(previousTimeLine);
+  const currentStatusKeys = filterStatusKeys(currentTimeLine);
+
+  if (previousStatusKeys.length !== currentStatusKeys.length) {
+    return true;
+  }
+
+  try {
+    for (const previousKey of previousStatusKeys) {
+      if (isEmptied(previousTimeLine, currentTimeLine, previousKey)) {
+        const stringList = previousKey.split(".");
+        const dateKey = stringList.slice(0, 2).join(".") + ".date";
+        const timeKey = stringList.slice(0, 4).join(".") + ".time";
+
+        if (checkDateTime(previousTimeLine, currentTimeLine, dateKey, timeKey)) {
+          // console.log("\n\n");
+          // console.log("There is a new empty place...");
+          // console.log("Theme Title:" + currentTimeLine["timeLineId"]);
+          // console.log("date:" + previousTimeLine[dateKey]);
+          // console.log("time:" + previousTimeLine[timeKey]);
+        }
+      }
+    }
+  } catch (error) {}
+};
+
+const checkDateTime = (previousTimeLine, currentTimeLine, dateKey, timeKey) =>
+  previousTimeLine[dateKey] == currentTimeLine[dateKey] &&
+  previousTimeLine[timeKey] == currentTimeLine[timeKey];
+
+const isEmptied = (previousTimeLine, currentTimeLine, previousKey) =>
+  previousTimeLine[previousKey] == AvailableStatus.NOT_AVAILABLE &&
+  currentTimeLine[previousKey] == AvailableStatus.AVAILABLE;
+
+const filterStatusKeys = (obj) => [
+  ...new Set(
+    Object.keys(obj).filter((val) => typeof val === "string" && val.match(/isAvailable$/))
+  ),
+];
 
 export { createInsertDataTask };
--- src/common/tools/flatten.js
+++ src/common/tools/flatten.js
@@ -1,27 +1,35 @@
-const flattenTimeLine = (timeLine) => {
+const flattenObjectList = (obj) => {
   const results = [];
-  results.push("timeLineId");
-  results.push(timeLine.timeLineId);
+
+  for (const [key, value] of Object.entries(obj)) {
+    results.push(key);
+    results.push(value);
+  }
+
+  return results;
+};
+
+const flattenTimeLineObject = (timeLine) => {
+  const results = {};
+  results["timeLineId"] = timeLine.timeLineId;
 
   const timeSlotList = timeLine.timeSlotList;
   for (var timeSlotIdx = 0; timeSlotIdx < timeSlotList.length; timeSlotIdx++) {
     const timeSlot = timeSlotList[timeSlotIdx];
     const timeSlotKey = `timeSlotList.[${timeSlotIdx}]`;
-    results.push(`${timeSlotKey}.date`);
-    results.push(timeSlot.date);
+
+    results[`${timeSlotKey}.date`] = timeSlot.date;
 
     const timeList = timeSlot.timeList;
     for (var timeIdx = 0; timeIdx < timeList.length; timeIdx++) {
       const time = timeList[timeIdx];
       const timeKey = `${timeSlotKey}.timeList.[${timeIdx}]`;
-      results.push(`${timeKey}.time`);
-      results.push(time.time);
-      results.push(`${timeKey}.isAvailable`);
-      results.push(time.isAvailable);
+      results[`${timeKey}.time`] = time.time;
+      results[`${timeKey}.isAvailable`] = time.isAvailable;
     }
   }
 
   return results;
 };
 
-export default flattenTimeLine;
+export { flattenObjectList, flattenTimeLineObject };
