30bd87d2b086e82402e14e568f830de6869bda46
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-19 11:07:11 +0900

    [fix] 해시 키 생성 메소드 생성

--- src/common/tools/hashCreator.js
+++ src/common/tools/hashCreator.js
@@ -2,6 +2,6 @@ import crypto from "crypto";
 
 const createHash = (data) => crypto.createHash("sha256").update(JSON.stringify(data)).digest("hex");
 
-const createHashKey = (venue, bid) => `${venue}_${bid}_hash`;
+const createTimeLineId = (themeTitle, originalUrl) => `${themeTitle}_${originalUrl}`;
 
-export { createHash, createHashKey };
+export { createHash, createTimeLineId };
